688de21a36462caea767d6423e34129fe36b0d06
Jonathan Apodaca <jrapodaca@gmail.com>
2018-08-30 10:50:37 -0600

    Install git as a prerequisite before running ansible-galaxy install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Install ansible
 sudo apt-get update
-sudo apt-get install -y software-properties-common unzip
+sudo apt-get install -y git software-properties-common unzip
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
