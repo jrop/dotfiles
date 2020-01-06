204ee4c1d1e699e7e5d9b69d6c8d9da36d62107d
Jonathan Apodaca <jrapodaca@gmail.com>
2020-01-06 08:33:13 -0700

    Make bw wrapper better

--- .config/fish/functions/bw.fish
+++ .config/fish/functions/bw.fish
@@ -1,45 +1,48 @@
-# Defined in /var/folders/my/bywv_hmn4g1_wwx511dvydzm0000gn/T//fish.6mhJHI/bw.fish @ line 2
 function bw
-	set bw_path (command -s bw)
-    if test -n bw_path
-        if test "$argv[1]" = "fzf"
-            #
-            # FZF
-            #
-            set selected_line (
-                eval $bw_path list items \
-                    | jq '.[] | "\(.name) | username: \(.login.username) | id: \(.id)" ' \
-                    | fzf --preview "echo {} | awk '{print \$(NF -0)}' | sed 's/\"//g' | xargs bw get item | jq '.'" \
-            )
-            if test -z $selected_line
-                echo error: `bw fzf` cancelled
-                return
-            end
-
-            echo $selected_line \
-                | awk '{print $(NF -0)}' \
-                | sed 's/\"//g' \
-                | xargs $bw_path get item \
-                | jq '.login.password' \
-                | sed 's/\"//g' \
-                | xargs tmux set-buffer
-            echo Copied password to tmux buffer
-        else if test "$argv[1]" = "unlock"
-            #
-            # Unlock
-            #
-            set key (eval $bw_path unlock --raw)
-            echo $key
-            export BW_SESSION=$key
-        else
-            #
-            # Everything else...
-            #
-            eval $bw_path $argv
+    set bw_path (command -s bw)
+    if test -z $bw_path
+        echo "Bitwarden is not installed"
+        read -P "Install @bitwarden/cli? [yN] " confirm
+        switch $confirm
+            case 'y' 'Y'
+                npm install --global @bitwarden/cli
+            case '*'
+                return 1
         end
+    end
+
+    if test "$argv[1]" = "fzf"
+        #
+        # FZF
+        #
+        set selected_line (
+            eval $bw_path list items \
+                | jq '.[] | "\(.name) | username: \(.login.username) | id: \(.id)" ' \
+                | fzf --preview "echo {} | awk '{print \$(NF -0)}' | sed 's/\"//g' | xargs bw get item | jq '.'" \
+        )
+        if test -z $selected_line
+            echo error: `bw fzf` cancelled
+            return
+        end
+
+        echo $selected_line \
+            | awk '{print $(NF -0)}' \
+            | sed 's/\"//g' \
+            | xargs $bw_path get item \
+            | jq '.login.password' \
+            | sed 's/\"//g' \
+            | xargs tmux set-buffer
+        echo Copied password to tmux buffer
+    else if test "$argv[1]" = "unlock"
+        #
+        # Unlock
+        #
+        set key (eval $bw_path unlock --raw)
+        export BW_SESSION=$key
     else
-        # TODO: Prompt to install
-        echo `bw` is not installed...
-        return 1
+        #
+        # Everything else...
+        #
+        eval $bw_path $argv
     end
 end
